c4987bf4ccc6fdf3db5ef7a76009bf11efaace7a
marcocosta55 <marcocosta55@gmail.com>
2020-03-10 14:42:26 -0400

    Getting recyclerview pn to work.

--- app/src/main/java/net/marco/msgrammarprototypesingleactivity/MainActivity.kt
+++ app/src/main/java/net/marco/msgrammarprototypesingleactivity/MainActivity.kt
@@ -4,29 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var imagesPN = arrayOf<Int>(
-        R.drawable.card_pn_luis,
-        R.drawable.card_pn_tomas,
-        R.drawable.card_pn_monica,
-        R.drawable.card_pn_luis_monica,
-        R.drawable.card_pn_monica_rosa,
-        R.drawable.card_pn_tomas_luis
-    )
-
     lateinit var recyclerViewPN: RecyclerView
-    lateinit var layoutManagerPN: RecyclerView.LayoutManager
+    private lateinit var layoutManagerPN: LinearLayoutManager
+    private lateinit var adapter : RecyclerPNAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerViewPN = findViewById<RecyclerView>(R.id.recycler_pn)
         layoutManagerPN = LinearLayoutManager(this)
-        recyclerViewPN.setHasFixedSize(true)
-        recyclerViewPN.layoutManager = layoutManagerPN
+        recycler_pn.layoutManager = layoutManagerPN
+
+        adapter = RecyclerPNAdapter()
+        recycler_pn.adapter = adapter
+
 
     }
 }
--- app/src/main/java/net/marco/msgrammarprototypesingleactivity/RecyclerPNAdapter.kt
+++ app/src/main/java/net/marco/msgrammarprototypesingleactivity/RecyclerPNAdapter.kt
@@ -1,18 +1,44 @@
 package net.marco.msgrammarprototypesingleactivity
 
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
-class RecyclerPNAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class RecyclerPNAdapter : RecyclerView.Adapter<RecyclerPNAdapter.ViewHolder>() {
+
+    private val imagesPN = arrayOf<Int>(
+        R.drawable.card_pn_luis,
+        R.drawable.card_pn_tomas,
+        R.drawable.card_pn_monica,
+        R.drawable.card_pn_luis_monica,
+        R.drawable.card_pn_monica_rosa,
+        R.drawable.card_pn_tomas_luis
+    )
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var itemImage: ImageView
+
+        init {
+            itemImage = itemView.findViewById(R.id.card_pn_layout)
+
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_pn_layout, viewGroup, false)
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return imagesPN.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemImage.setImageResource(imagesPN[i])
     }
 }
